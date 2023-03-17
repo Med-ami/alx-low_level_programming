277cabf18a24e4232568bfce80d34140927296d4
Amine <beispiel@example.com>
2023-03-16 18:08:18 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,10 @@
 
 int main(void)
 {
-	char quo[] = " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	 char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(1, quo, 59);
+	write(2, message, 38);
 	return (1);
 }
+
+
