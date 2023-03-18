c1dfd0e3fef7a76e6d5a46d23565b47f2ee63870
Amine <beispiel@example.com>
2023-03-18 08:07:02 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ *
+ *Description: A C program that prints with put function
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+	printf("%d\n", i);
+	i++;
+	}
+	return (0);
+}
