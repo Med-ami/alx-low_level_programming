8bf1ecc938e5605759c629f8228747d88184c168
Amine <beispiel@example.com>
2023-03-19 06:27:15 -0700

    Patience\, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Description: A C program that prints with put function
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	putchar(i + 48);
+	if (i != 9)
+	{	
+		putchar(44);
+		putchar(32);
+	}
+	}
+	return (0);
+}
+
