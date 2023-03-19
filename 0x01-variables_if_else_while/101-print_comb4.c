ac1bc9da3ce76d57c3516c91918c6e8f7c7b3058
Amine <beispiel@example.com>
2023-03-19 08:55:52 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Description: A C program that prints with put function
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 9; i++)
+	{
+	for (j = i + 1; j < 9 ; j++)
+	{
+	for (k = i + 2; k <= 9 ; k++)
+	{
+	putchar(i + 48);
+	putchar(j + 48);
+	putchar(k + 48);
+	if (i != 7 || j != 8 || k != 9)
+	{
+	putchar(44);
+	putchar(32);
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+
