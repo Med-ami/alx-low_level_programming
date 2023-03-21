8162c82effd2ff03f6a031ff20ba6b1920dda3e5
Amine <beispiel@example.com>
2023-03-21 16:47:35 -0700

    hour min

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int h, min;
+
+	h = 0;
+
+	while (h < 24)
+	{
+		min = 0;
+		while (min < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+			min++;
+		}
+		h++;
+	}
+}
